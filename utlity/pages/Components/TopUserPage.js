0944eb176acfcf8f8d3e43ebbbcb82a1d9e76168
Nipun753 <71968869+Nipun753@users.noreply.github.com>
2022-07-01 03:11:38 -0700

    Update TopUserPage.js

--- utlity/pages/Components/TopUserPage.js
+++ utlity/pages/Components/TopUserPage.js
@@ -2,10 +2,11 @@ import React, { useContext } from 'react'
 import { Table } from 'react-bootstrap'
 import {TopUser,SetTopUser} from '../index'
 export default function TopUserPage() {
-    const topUser=useContext(TopUser)
+    const topUser=useContext(TopUser);
+     const setTopUser =useContext(SetTopUser)
   return (
-    <>
-    <h1 className='text-black'>Top Users</h1>
+     <>
+    <h1 style={{marginLeft:"300px",width:"70%"}} className='text-black text-center mb-3'>Top Users</h1>
     <Table className=' new ' style={{marginLeft:"300px",width:"70%"}} striped bordered hover>
                 <thead>
                     <tr>
@@ -13,6 +14,7 @@ export default function TopUserPage() {
                         <th>First Name</th>
                         <th>Username</th>
                         <th>Email</th>
+                        <th>Remove Top User</th>
                        
                     </tr>
                 </thead>
@@ -23,9 +25,14 @@ export default function TopUserPage() {
                             <td>{sin.name} </td>
                             <td>{sin.username}</td>
                             <td>{sin.email}</td>
+                            <td><input type='checkbox' checked={false} onChange={(e) =>{
+                                if(e.target.checked===true){               
+                                    (topUser.splice(idx,1))
+                                    setTopUser([...topUser])
+                                }
+                            }} ></input></td>
                         </tr>
                     ))}
-
                 </tbody>
             </Table>
     </>
